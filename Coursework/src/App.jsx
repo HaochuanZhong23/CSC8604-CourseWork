36ad320f803d2237aec25e8460e9ba7215dd9555
haochuan <haochuan607@126.com>
2024-03-12 15:04:12 +0000

    update

--- Coursework/src/App.jsx
+++ Coursework/src/App.jsx
@@ -1,66 +1,16 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Result from './Result';
+import Suggestion from './Suggestion';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [temp, setTemp] = useState(null)
-
-  useEffect(() => {
-    async function fetchData(url) {
-      try {
-        const res = await fetch(`http://localhost:3000${url}`, {
-          //mode: 'no-cors',
-          method: 'get',
-          headers: {
-            "Content-Type": "application/json"
-          },
-        })
-        console.log(JSON.stringify(res))
-        if (!res.ok) {
-          throw new Error('Network response was not ok')
-        }
-        const jsonData = await res.json();
-        return jsonData;
-      } catch (error) {
-        console.error(`Error fetching data from ${url}:`, error)
-        return [];
-      }
-    }
-
-    async function getData() {
-      const temperature = await fetchData('/temp')
-      console.log(temperature)
-      setTemp(temperature)
-    }
-
-    getData()
-  }, [])
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        {temp}
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Result}></Route>
+        <Route path='/suggestion' Component={Suggestion}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
